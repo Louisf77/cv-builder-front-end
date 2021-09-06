59ea526585c77a45ebac844d59db84dd52a631b5
Louis Freeman <louis.freeman@gmail.com>
2021-09-06 10:35:45 +0100

    Addition of greeting page formatting

--- src/components/Login & greet/LoginScreen.tsx
+++ src/components/Login & greet/LoginScreen.tsx
@@ -1,38 +1,81 @@
-import { Box, Center, Heading, Image, VStack } from "@chakra-ui/react";
+import { Box, Button, Center, chakra, Heading, Icon, Image, Stack, Text, useColorModeValue, VStack } from "@chakra-ui/react";
 import SignupButton from "../Auth/SignupButton";
 import logo from "../../utils/quikcvLogo.png";
 import icon from "../../utils/quikcvIcon.png";
+import screenShot from "../../utils/screenshotForLogin.png"
 
 export default function LoginScreen(): JSX.Element {
   return (
     <>
-      <Center h="100vh" w="100vw">
-        <VStack>
+   
+        <VStack paddingTop="20px">
           <Image src={icon} width="135.125px" />
           <Image src={logo} width="540.5px" align="center" />
-          <Box
-            width="700px"
-            borderRadius="15"
-            margin="auto"
-            height="200px"
-            boxShadow="md"
-            backgroundColor="white"
-            paddingTop="50px"
-            align="center"
+  
+      <Box px={8} py={24} mx="auto">
+      <Box
+        w={{ base: "full", md: 11 / 12, xl: 9 / 12 }}
+        mx="auto"
+        textAlign={{ base: "left", md: "center" }}
+      >
+        <chakra.h1
+          mb={6}
+          fontSize={{ base: "4xl", md: "6xl" }}
+          fontWeight="bold"
+          lineHeight="none"
+          letterSpacing={{ base: "normal", md: "tight" }}
+          color={useColorModeValue("gray.900",'gray.100')}
+        >
+          Everything {" "}
+          <Text
+            display={{ base: "block", lg: "inline" }}
+            w="full"
+            bgClip="text"
+            bgGradient="linear(to-r, green.400,purple.500)"
+            fontWeight="extrabold"
           >
-            <Heading
-              align="center"
-              fontSize="20px"
-              fontWeight="medium"
-              marginBottom="50px"
-            >
-              SIGN IN OR CREATE AN ACCOUNT TO GET STARTED
-            </Heading>
-
-            <SignupButton />
-          </Box>
-        </VStack>
-      </Center>
+            about you
+          </Text>{" "}
+          in one single place.
+        </chakra.h1>
+        <chakra.p
+          px={{ base: 0, lg: 24 }}
+          mb={6}
+          fontSize={{ base: "lg", md: "xl" }}
+          color={useColorModeValue("gray.600",'gray.300')}
+        >
+          QuikCV is an all in one CV building tool, with a focus on ease of use and creating an easy to share clean CV that employers can't say no to.
+        </chakra.p>
+        <Stack
+        direction={{base:"column",sm:"row"}}
+          mb={{ base: 4, md: 8 }}
+          spacing={2}
+          justifyContent={{ sm: "left", md: "center" }}
+        >
+           <SignupButton />
+          
+        </Stack>
+      </Box>
+      <Box
+        w={{ base: "full", md: 10 / 12 }}
+        mx="auto"
+        mt={20}
+        textAlign="center"
+      >
+        <Image
+          w="full"
+          rounded="lg"
+          shadow="2xl"
+          src={screenShot}
+          alt="Hellonext feedback boards software screenshot"
+        />
+      </Box>
+    </Box>
+    </VStack>
+  
     </>
+    
   );
 }
+
+

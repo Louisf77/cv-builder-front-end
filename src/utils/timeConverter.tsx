c71df38b56dea72fab56b68bf8c028ebd44a7c05
Louis Freeman <louis.freeman@gmail.com>
2021-09-08 11:33:59 +0100

    fix date formatter

--- src/utils/timeConverter.tsx
+++ src/utils/timeConverter.tsx
@@ -1,8 +1,8 @@
 export function timeConverter(time: string): string {
   const t = time.split("T")[0];
   const dateNums = t.split("-");
-  const m = parseInt(dateNums[1]);
-  const d = parseInt(dateNums[2]);
+  const m = parseInt(dateNums[1]) - 1;
+  const d = parseInt(dateNums[2]) + 1;
   const date = new Date(parseInt(dateNums[0]), m, d);
   const month = date.toLocaleString("default", { month: "long" });
   return `${month} ${dateNums[0]} `;
